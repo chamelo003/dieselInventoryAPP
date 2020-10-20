59d5654a8f8e505a76dce25c2768000feb6cd4af
chamelo <chamelo03@gmail.com>
2020-10-20 15:45:13 -0600

    creada modal y scripts para data de modal

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,7 +3,18 @@
 *   En este archivo van todos los scripts necesarios para   *
 *   interactuar con el DOM.                                 *
 ************************************************************/
-$(document).ready( function () {
-    $('#prueba').DataTable();
-} );
+$(document).ready( function (){
+
+    // funcion para llenar el transportista al ingresar un motorista.
+    $('.RTN').dblclick(function(){
+        // Asignamos el RTN seleccionado a la variable RtnTrans
+        RtnTrans = $(this).text();
+        // Quitamos todos los espacios que tenga la cadena de texto.
+        RtnTrans = RtnTrans.replace(/\s/g,'');
+        //alert(RtnTrans);
+        // Asignamos el RTN seleccionado y sin espacios a la textbox correspondiente.
+        $('#Transportista').val(RtnTrans);
+    });
+
+});
 
